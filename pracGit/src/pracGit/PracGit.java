d3ad7fe917867f7cc52b9719b072d67441463e87
Veganark <Veganark@192.168.1.36>
2019-01-06 22:40:07 +0100

    No puc fer una branca amb un compte diferent. Provaré amb el meu principal.

--- pracGit/src/pracGit/PracGit.java
+++ pracGit/src/pracGit/PracGit.java
@@ -7,6 +7,8 @@ public class PracGit {
 		System.out.print("Vaig a provar fer una rama i pujar-la amb un compte de Git diferent.");
 		System.out.print("Però com que estic gastant un sol ordinador, no sé com eixirà la cosa.");
 		System.out.print("Això no està anant com toca, perquè s'ha de fer des de diferents ordinadors.");
+		System.out.print("A més les opcions de branca no són les mateixes que al PDF. Supose que perquè només sóc una persona.");
+
 
 	}
 }
