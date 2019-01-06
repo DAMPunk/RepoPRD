6a239fff62ad375693976ac21ebb8916d91a8647
Veganark <Veganark@192.168.1.36>
2019-01-06 22:39:32 +0100

    No puc fer una branca amb un comte diferent. Provaré amb el meu propi.

--- pracGit/src/pracGit/PracGit.java
+++ pracGit/src/pracGit/PracGit.java
@@ -6,6 +6,7 @@ public class PracGit {
 		System.out.print("Hola, Ricardo");
 		System.out.print("Vaig a provar fer una rama i pujar-la amb un compte de Git diferent.");
 		System.out.print("Però com que estic gastant un sol ordinador, no sé com eixirà la cosa.");
+		System.out.print("Això no està anant com toca, perquè s'ha de fer des de diferents ordinadors.");
 
 	}
 }
