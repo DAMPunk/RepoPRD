6ad3a3ffad7a437ce9bd9ee4bbfadfb4afe4618b
Veganark <Veganark@192.168.1.36>
2019-01-06 22:32:17 +0100

    Prove pujar la rama amb altre compte de Git

--- pracGit/src/pracGit/PracGit.java
+++ pracGit/src/pracGit/PracGit.java
@@ -4,5 +4,8 @@ package pracGit;
 public class PracGit {
 	public static void main (String[] args) {
 		System.out.print("Hola, Ricardo");
+		System.out.print("Vaig a provar fer una rama i pujar-la amb un compte de Git diferent.");
+		System.out.print("Però com que estic gastant un sol ordinador, no sé com eixirà la cosa.");
+
 	}
 }
